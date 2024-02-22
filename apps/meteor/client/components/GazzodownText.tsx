5f45ff1b551ce79c221b8d690b73cc7c6be94721
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-02-21 21:23:54 -0300

    add timestamp feature preview

--- apps/meteor/client/components/GazzodownText.tsx
+++ apps/meteor/client/components/GazzodownText.tsx
@@ -13,6 +13,7 @@ import { fireGlobalEvent } from '../lib/utils/fireGlobalEvent';
 import { useUserCard } from '../views/room/contexts/UserCardContext';
 import { useGoToRoom } from '../views/room/hooks/useGoToRoom';
 import { useMessageListHighlights } from './message/list/MessageListContext';
+import { useFeaturePreview } from '@rocket.chat/ui-client';
 
 type GazzodownTextProps = {
 	children: JSX.Element;
